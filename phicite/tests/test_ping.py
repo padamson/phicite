6dae2f9c839108064553304f927f112c292df70f
Paul Adamson <paul.adamson.01@gmail.com>
2025-06-06 06:13:03 -0400

    break a test to check ci

--- phicite/tests/test_ping.py
+++ phicite/tests/test_ping.py
@@ -1,4 +1,4 @@
 def test_ping(test_app):
-    response = test_app.get("/ping")
+    response = test_app.get("/pings")
     assert response.status_code == 200
     assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
